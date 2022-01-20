8ebfbe4a48372b0ca77365c2cd855a245adb8034
amitpande <amit.pande2121@gmail.com>
2022-01-20 14:33:15 +0530

    created a new user

--- src/test/java/SampleTests.java
+++ src/test/java/SampleTests.java
@@ -6,11 +6,27 @@ import static io.restassured.RestAssured.given;
 public class SampleTests {
 
     @Test
-    public void ShouldGetAllUsers(){
+    public void ShouldGetAllUsers() {
         given()
                 .when().get("https://gorest.co.in/public/v1/users")
 
-                        .then().log().all().statusCode(200);
+                .then().log().all().statusCode(200);
+
+
+    }
+
+
+    @Test
+    public void ShouldCreateUser() {
+
+        given()
+                .header("Accept","application/json")
+                .header("Content-Type","application/json")
+                .header("Authorization","Bearer 77970b9d769496fe209c6251a9ca0ada2e88866ca6ece14b99b380154a3998d7")
+                .body("{\"name\":\"Tenali Ramakrishna\", \"gender\":\"male\", \"email\":\"tenali.ramakrishna@203.com\", \"status\":\"active\"}")
+
+                .when().post("https://gorest.co.in/public/v1/users")
+                .then().log().all().statusCode(201);
 
 
     }
