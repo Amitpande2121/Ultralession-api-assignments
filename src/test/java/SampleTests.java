ce9406ea58c2a8b44763460fdd8dbbea7bd23330
amitpande <amit.pande2121@gmail.com>
2022-01-20 10:02:46 +0530

    initial commit

---
+++ src/test/java/SampleTests.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTests {
+
+    @Test
+    public void ShouldAddSomething(){
+        Assert.assertEquals(1,1);
+    }
+}
