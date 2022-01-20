cbcc34316dde75252738090f42dd70cad4f591cb
amitpande <amit.pande2121@gmail.com>
2022-01-20 15:29:50 +0530

    Assertion added on responce body , id and email

--- src/test/java/SampleTests.java
+++ src/test/java/SampleTests.java
@@ -1,3 +1,4 @@
+import org.hamcrest.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -23,10 +24,11 @@ public class SampleTests {
                 .header("Accept","application/json")
                 .header("Content-Type","application/json")
                 .header("Authorization","Bearer 77970b9d769496fe209c6251a9ca0ada2e88866ca6ece14b99b380154a3998d7")
-                .body("{\"name\":\"Tenali Ramakrishna\", \"gender\":\"male\", \"email\":\"tenali.ramakrishna@203.com\", \"status\":\"active\"}")
+                .body("{\"name\":\"Tenali Ramakrishna\", \"gender\":\"male\", \"email\":\"tenali.ramakrishna@206.com\", \"status\":\"active\"}")
 
                 .when().post("https://gorest.co.in/public/v1/users")
-                .then().log().all().statusCode(201);
+                .then().log().all().statusCode(201).body("data.id", Matchers.notNullValue())
+                .body("data.email",Matchers.equalTo("tenali.ramakrishna@206.com"));
 
 
     }
